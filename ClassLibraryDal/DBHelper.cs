056b49d2a39304cb8a7cd64eafef7a60beb5ae8c
M-Aitisam <aitisamahmed24@gmail.com>
2024-07-20 11:18:58 +0500

    Add project files.

---
+++ ClassLibraryDal/DBHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ClassLibraryDal
+{
+    public class DBHelper
+    {
+        public static SqlConnection GetConnection()
+        {
+            SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=db_Student;Integrated Security=True");
+            return con;
+        }
+    }
+}
---
+++ ClassLibraryModel/ModelStudent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ClassLibraryModel
+{
+    public class ModelStudent
+    {
+        public int StuId { get; set; }
+
+        public string? FirstName { get; set; }
+
+        public string? EmailAddress { get; set; }
+    }
+}
